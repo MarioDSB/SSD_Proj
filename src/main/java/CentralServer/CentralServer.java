dd55d6757070a5696d575b392ea86e555dad79db
MarioDSB <mariodinis1997@gmail.com>
2019-05-05 16:39:49 +0100

    Separated Client RPC from Central Server RPC

--- src/main/java/CentralServer/CentralServer.java
+++ src/main/java/CentralServer/CentralServer.java
@@ -1,4 +1,69 @@
 package CentralServer;
 
+import GrupoB.gRPCService.ServerProto.*;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class CentralServer {
+    private static final Logger logger = Logger.getLogger(CentralServer.class.getName());
+
+    /* The port on which the server should run */
+    private final int PORT = 50051;
+    private Server server;
+
+    private void start() throws Exception {
+        server = ServerBuilder.forPort(PORT)
+                .addService(ServerGrpc.bindService(new ServerProtoImpl()))
+                .build()
+                .start();
+        logger.info("Server started, listening on " + PORT);
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+            System.err.println("*** shutting down gRPC server since JVM is shutting down");
+            CentralServer.this.stop();
+            System.err.println("*** server shut down");
+        }));
+    }
+
+    private void stop() {
+        if (server != null)
+            server.shutdown();
+    }
+
+    /**
+     * Await termination on the main thread since the grpc library uses daemon threads.
+     */
+    private void blockUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
+    }
+
+    /**
+     * Main launches the server from the command line.
+     */
+    public static void main(String[] args) throws Exception {
+        final CentralServer server = new CentralServer();
+        server.start();
+        server.blockUntilShutdown();
+    }
+
+    private class ServerProtoImpl implements ServerGrpc.Server {
+
+        @Override
+        public void join(NodeJoin request, StreamObserver<NetworkInfo> responseObserver) {
+
+        }
+
+        @Override
+        public void ping(EmptyMessage request, StreamObserver<BooleanMessage> responseObserver) {
+            logger.log(Level.INFO, "Receiving ping. Responding...");
+            responseObserver.onNext(BooleanMessage.newBuilder().setResult(true).build());
+            responseObserver.onCompleted();
+        }
+    }
 }
